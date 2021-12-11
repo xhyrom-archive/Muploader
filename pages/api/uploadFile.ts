38a646faf19bab333a87c514eb604f4297b87a9c
Hyro <generalkubo@gmail.com>
2021-12-11 23:40:11 +0100

    Update uploadFile.ts

--- pages/api/uploadFile.ts
+++ pages/api/uploadFile.ts
@@ -1,8 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-
-// @ts-ignore
-import * as formidable from 'formidable';
+import formidable from 'formidable';
 import axios from 'axios';
 
 type Data = {
