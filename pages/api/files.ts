eac06365c10a3cbff0a495de8cf6497711a0a9ae
xhyrom <generalkubo@gmail.com>
2021-12-12 10:08:28 +0100

    netlify node:fs

--- pages/api/files.ts
+++ pages/api/files.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import fs from 'fs';
+import * as fs from 'fs';
 
 type Data = {
     name: string;
