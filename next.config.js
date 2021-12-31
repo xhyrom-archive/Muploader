658503fdcc6e71bf085483d067d8912dd1dcde43
xhyrom <generalkubo@gmail.com>
2021-12-31 21:41:18 +0100

    remove plugin

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  target: "serverless",
-  webpack: (config, options) => {
-    const webpack = require('webpack')
-
-    config.plugins.push(
-      new webpack.ContextReplacementPlugin(/highlight\.js\/lib\/languages$/, new RegExp(`^./(javascript)$`))
-    )
-
-    return config;
-  }
+  target: "serverless"
 }
