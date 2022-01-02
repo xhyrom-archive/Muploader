b2042f586995b6e17926ff725b1c6d010586e448
xhyrom <generalkubo@gmail.com>
2022-01-02 15:01:20 +0100

    headers

--- next.config.js
+++ next.config.js
@@ -1,5 +1,22 @@
+const { createSecureHeaders } = require("next-secure-headers");
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  target: "serverless"
+  target: "serverless",
+  async headers() {
+    return [{
+      source: "/(.*)",
+      headers: createSecureHeaders({
+        contentSecurityPolicy: {
+          directives: {
+            defaultSrc: "'self'",
+            styleSrc: ["'self'"],
+          },
+        },
+        forceHTTPSRedirect: [true, { maxAge: 60 * 60 * 24 * 4, includeSubDomains: true }],
+        referrerPolicy: "same-origin",
+      }),
+    }];
+  }
 }
--- tests/getdepends.mjs
+++ tests/getdepends.mjs
@@ -1,7 +1,7 @@
 import fs from 'fs';
 
 const json = Object.entries(JSON.parse(fs.readFileSync('./package.json', 'utf-8')).dependencies);
-const jsonDev = Object.entries(JSON.parse(fs.readFileSync('./package.json', 'utf-8')).dependencies);
+const jsonDev = Object.entries(JSON.parse(fs.readFileSync('./package.json', 'utf-8')).devDependencies);
 
 console.log(json.map(d => `npm i ${d[0]}`).join(' && '))
 console.log(jsonDev.map(d => `npm i ${d[0]} --save-dev`).join(' && '))
